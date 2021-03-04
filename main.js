42b67fb3c6a7cb9de6b11e0fe03dbc81fc15780c
wass01 <maatallawassim01@gmail.com>
2021-03-04 15:30:55 +0100

    ESERCIZIO TERMINATO

--- main.js
+++ main.js
@@ -16,3 +16,7 @@ listaCognomi.sort();
 for (var i = 0; i < listaCognomi.length; i++) {
   document.getElementById('cognomi').innerHTML += "<li>" + listaCognomi[i] + "</li>";
 }
+
+for (var j = 0; j < listaCognomi.length; j++) {
+  console.log(listaCognomi[j] + " si trova nella posizione numero: " + (j + 1));
+}
